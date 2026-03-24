8839779dc8b15fcbadcc38aa60880dd8fcb7d5a4
andresZam12 <giovanny.zambranob@campusucc.edu.co>
2026-03-24 16:23:37 -0500

    creacion clases de retorno di no encuentra datos, separacion de responsabilidades

--- veri_farmac/lib/features/medicine_detail/domain/repositories/i_medicine_repository.dart
+++ veri_farmac/lib/features/medicine_detail/domain/repositories/i_medicine_repository.dart
@@ -1,2 +1,16 @@
 // Contrato del repositorio de medicamentos.
 // TODO: definir getByBarcode, getByOcrText, getByRegistryCode, searchByName
+
+import '../entities/medicine.dart';
+
+// Contrato que define qué puede hacer el repositorio de medicamentos.
+abstract class IMedicineRepository {
+  // Busca un medicamento por código de barras
+  Future<Medicamento?> buscarPorBarcode(String barcode);
+
+  // Busca un medicamento por registro sanitario INVIMA
+  Future<Medicamento?> buscarPorRegistro(String registro);
+
+  // Busca medicamentos por nombre o ingrediente activo
+  Future<List<Medicamento>> buscarPorTexto(String texto);
+}
--- veri_farmac/lib/features/medicine_detail/domain/usecases/get_medicine_usecase.dart
+++ veri_farmac/lib/features/medicine_detail/domain/usecases/get_medicine_usecase.dart
@@ -1,2 +1,29 @@
 // Casos de uso: obtener medicamento por barcode u OCR.
 // TODO: implementar GetMedicineByBarcodeUseCase y GetMedicineByOcrUseCase
+
+import '../entities/medicine.dart';
+import '../repositories/i_medicine_repository.dart';
+
+// Obtiene la información completa de un medicamento por barcode.
+class GetMedicineByBarcodeUseCase {
+  const GetMedicineByBarcodeUseCase(this._repo);
+  final IMedicineRepository _repo;
+
+  Future<Medicamento?> call(String barcode) => _repo.buscarPorBarcode(barcode);
+}
+
+// Obtiene la información completa de un medicamento por registro INVIMA.
+class GetMedicineByRegistroUseCase {
+  const GetMedicineByRegistroUseCase(this._repo);
+  final IMedicineRepository _repo;
+
+  Future<Medicamento?> call(String registro) => _repo.buscarPorRegistro(registro);
+}
+
+// Busca medicamentos por nombre o ingrediente activo.
+class SearchMedicineUseCase {
+  const SearchMedicineUseCase(this._repo);
+  final IMedicineRepository _repo;
+
+  Future<List<Medicamento>> call(String texto) => _repo.buscarPorTexto(texto);
+}
