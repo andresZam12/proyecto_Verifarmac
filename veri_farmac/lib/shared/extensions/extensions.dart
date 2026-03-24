e838006273b771d5cecc79e6801a580d6d43bfe7
DeividJulian <deivid.alvarado@campusucc.edu.co>
2026-03-24 00:38:20 -0500

    Importando dependencias

--- veri_farmac/lib/shared/extensions/extensions.dart
+++ veri_farmac/lib/shared/extensions/extensions.dart
@@ -1,2 +1,8 @@
 // Barrel export de todas las extensiones del proyecto.
-// TODO: exportar string_extensions, datetime_extensions y context_extensions
+// Exporta todas las extensiones en un solo import.
+// En vez de importar cada archivo por separado, solo usas:
+// import 'package:verifarmac/shared/extensions/extensions.dart';
+
+export '../../core/utils/extensions/string_extensions.dart';
+export '../../core/utils/extensions/datetime_extensions.dart';
+export '../../core/utils/extensions/context_extensions.dart';
